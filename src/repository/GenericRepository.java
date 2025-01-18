ac6617c198073cc1630c11e09b6cc132fabbf17b
Aynurmm <aynurmammadova09@gmail.com>
2025-01-18 13:35:24 +0400

    GenericRepository

--- src/repository/GenericRepository.java
+++ src/repository/GenericRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
-public interface GenericRepository {
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericRepository<T> {
+
+    Optional<T> findById(Long id);
+    List<T> findAll();
+    void save(T entity);
+    void update(Long id, T entity);
+    void delete(Long id);
 }
