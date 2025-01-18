7ad25a5f484bbf4687cc12f0fbe19673dfa14876
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-01-18 13:38:27 +0400

    master

--- src/repository/OrderRepository.java
+++ src/repository/OrderRepository.java
@@ -1,4 +1,33 @@
 package repository;
 
-public interface OrderRepository {
+import model.Order;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OrderRepository implements GenericRepository<Order>{
+    @Override
+    public Optional<Order> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void save(Order entity) {
+
+    }
+
+    @Override
+    public void update(Long id, Order entity) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,4 +1,34 @@
 package repository;
 
-public interface UserRepository {
+import model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserRepository implements GenericRepository<User> {
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void save(User entity) {
+
+    }
+
+    @Override
+    public void update(Long id, User entity) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
