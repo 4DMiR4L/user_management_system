e5d46b783c3fd499f198f68f0edcc319ea4d04ee
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-01-18 14:21:21 +0400

    master

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -2,21 +2,15 @@ package repository;
 
 import model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.ArrayList;
 
 public class UserRepository implements GenericRepository<User> {
+
     private List<User> users = new ArrayList<>();
-
-
     @Override
     public Optional<User> findById(Long id) {
-        for (User user : users) {
-            if (user.getId().equals(id)) {
-                return Optional.of(user);
-            }
-        }
         return Optional.empty();
     }
 
