ede2fb128603998507c50b91eb09a54ff652fc4d
4DMiR4L <153876304+4DMiR4L@users.noreply.github.com>
2025-01-18 13:07:24 +0400

    master

--- src/model/BaseEntity.java
+++ src/model/BaseEntity.java
@@ -2,4 +2,16 @@ package model;
 
 public class BaseEntity {
     private Long id;
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
