5d45099e6ff3753fa636e351d78e7f0a1c096ed0
Aynurmm <aynurmammadova09@gmail.com>
2025-01-18 14:17:23 +0400

    crud

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -22,22 +22,43 @@ public class UserRepository implements GenericRepository<User> {
 
     @Override
     public List<User> findAll() {
-
+        List<User> allUsers = new ArrayList<>();
+        for (User user : users) {
+            allUsers.add(user);
+        }
+        return allUsers;
     }
 
     @Override
     public void save(User entity) {
-
+        users.add(entity);
     }
 
     @Override
     public void update(Long id, User entity) {
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            if (user.getId().equals(id)) {
+                users.set(i, entity);
+                System.out.println("User with ID " + id + " updated successfully.");
+                return;
+            }
+        }
+        System.out.println("User with ID " + id + " not found.");
 
     }
 
     @Override
     public void delete(Long id) {
-
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            if (user.getId().equals(id)) {
+                users.remove(i);
+                System.out.println("User with ID " + id + " deleted successfully.");
+                return;
+            }
+        }
+        System.out.println("User with ID " + id + " not found.");
     }
 
 }
